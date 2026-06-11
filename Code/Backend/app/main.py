8443624b4e4b034dd594869b0146f9f74c9b660f
Angus Tan <tvhang7@gmail.com>
2026-06-11 16:30:42 +0800

    fix: update CORS middleware to include production URL

--- Code/Backend/app/main.py
+++ Code/Backend/app/main.py
@@ -81,7 +81,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://project-mini-map.vercel.app"
     ],
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
